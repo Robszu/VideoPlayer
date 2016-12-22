b772d6acaea0e8c0c07510a7b614c35e2791a03d
Robszu <robert.szuchnik@gmail.com>
2016-12-22 16:17:03 +0100

    JS functions and eventListeners

--- script.js
+++ script.js
@@ -4,9 +4,68 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 /* Build out functions */
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+  // if (video.paused) {
+  //   video.play();
+  // } else {
+  //   video.pause();
+  // }
+}
+function updateButton() {
+  const icon = this.paused ?  '►' : '❚ ❚';
+  console.log(icon);
+  toggle.textContent = icon;
+  //console.log('Update the button');
+}
+function skip() {
+  console.log(this.dataset);
+  console.log(this.dataset.skip);
+  console.log(video.currentTime);
+  video.currentTime += Number(this.dataset.skip);
+}
+function handleRangeUpdate() {
+  // console.log(this.name);
+  // console.log(video.volume);
+  // console.log(video.playbackRate);
+  // console.log(this.value);
+  video[this.name] = this.value;
+  // if (this.name === 'volume') {
+  //   video.volume = this.value;
+  // } else {
+  //   video.playbackRate = this.value;
+  // }
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
 
 /* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
